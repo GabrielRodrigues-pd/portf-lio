13bfa0ef28932e168a08d3ac8cada9aeeb1eed6c
gabriel <gabriel.r.lacerda@hotmail.com>
2023-03-08 13:27:36 -0300

    baixar cv

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -36,7 +36,7 @@ export function About() {
               Contato
             </a>
             <a
-              href="https://drive.google.com/file/d/1-0-xckQtclegYldlY6kdFUeqbdkACYTz/view?usp=share_link"
+              href="/src/arquivo/CV.txt"
               download="Currículo Virtual"
               target="_blank"
             >
